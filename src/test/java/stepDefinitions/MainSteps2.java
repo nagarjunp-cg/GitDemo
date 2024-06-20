fa4f6c7294ea788021a1d806da468f5f9f55b786
nagarjunp-cg <nagarjun.p@capgemini.com>
2024-06-20 10:47:20 +0530

    Asain Timezone guy commit develop

--- src/test/java/stepDefinitions/MainSteps2.java
+++ src/test/java/stepDefinitions/MainSteps2.java
@@ -4,30 +4,29 @@ import io.cucumber.java.en.Given;
 
 public class MainSteps2 {
 
-	
-	
 	public void mainsteps2method1() {
-		
+
 		System.out.println("Step1");
 	}
 
-	
-	
 	public void mainsteps2method2() {
-		
+
 		System.out.println("Step2");
 	}
 
 	public void mainsteps2method3() {
-		
+
 		System.out.println("step3");
 	}
 
-public void mainsteps2method4() {
-		
+	public void mainsteps2method4() {
+
 		System.out.println("step4");
 	}
-	
-	
+
+	public void mainsteps2method5() {
+
+		System.out.println("step5 added by Asian TimeZoneguy develop");
+	}
 	
 }
